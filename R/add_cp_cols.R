993407fd915e7128f5e740ce80af6ba8bb3f5376
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-11-19 14:49:03 -0600

    check whether station_info exists within the bleutils package

--- R/add_cp_cols.R
+++ R/add_cp_cols.R
@@ -21,7 +21,7 @@ add_cp_cols <- function(df, station_code_col) {
 update_cp_stations <- function(source_file = "C:/Users/atn893/Documents/Box Sync/Beaufort LTER/Core Program/BLE_LTER_CP_Stations.xlsx",
                                sheet = "lookup") {
   canon <- readxl::read_excel(source_file, sheet = sheet)
-  if (all(station_info == canon)) message("Package version of station info is up-to-date. No actions taken.")
+  if (exists("station_info", where = "package:bleutils") && all(bleutils::station_info == canon)) message("Package version of station info is up-to-date. No action taken.")
   else {
     message("Nope. Something's changed. Updating package version of station info.")
     station_info <- canon
