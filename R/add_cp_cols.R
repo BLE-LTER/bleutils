0c23a5d6cebb47b9ace0a359c35f0d82f14f0b2c
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-12-03 15:34:03 -0600

    better comparison between canon and pkg versions of station info

--- R/add_cp_cols.R
+++ R/add_cp_cols.R
@@ -1,11 +1,11 @@
 #' Add columns relevant to BLE LTER Core Program
 #' Add columns: station name, lat/lon, habitat type, given BLE LTER Core Program station code
 #' @param df (data.frame) Data frame in question with CP data
-#' @param station_code_col (character) Name of column containing BLE LTER station codes
+#' @param station_code_col (character) Name of column containing BLE LTER station codes in supplied df. Defaults to "station".
 #' @return Data with new station name, lat/lon, habitat type columns appended
 #' @export
 
-add_cp_cols <- function(df, station_code_col) {
+add_cp_cols <- function(df, station_code_col = "station") {
   # TODO: check existing data for columns first before merging
 
   df <- merge(df, bleutils::station_info, by.x = station_code_col, by.y = "station_id")
@@ -21,7 +21,7 @@ add_cp_cols <- function(df, station_code_col) {
 update_cp_stations <- function(source_file = "C:/Users/atn893/Documents/Box Sync/Beaufort LTER/Core Program/BLE_LTER_CP_Stations.xlsx",
                                sheet = "lookup") {
   canon <- readxl::read_excel(source_file, sheet = sheet)
-  if (exists("station_info", where = "package:bleutils") && all(bleutils::station_info == canon) && all(colnames(station_info) == colnames(canon))) message("Package version of station info is up-to-date. No action taken.")
+  if (exists("station_info", where = "package:bleutils") && isTRUE(all.equal(bleutils::station_info, canon)) && all(colnames(station_info) == colnames(canon))) message("Package version of station info is up-to-date. No action taken.")
   else {
     message("Nope. Something's changed. Updating package version of station info.")
     station_info <- canon
