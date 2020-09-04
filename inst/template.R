3052aaf55d36887841967338cab37ca46b79321d
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2020-09-04 05:32:52 -0500

    replace "localhost" with ip

--- inst/template.R
+++ inst/template.R
@@ -15,7 +15,7 @@ library(bleutils)
 # type in username and password in the R console
 metadata <- append_units(get_meta(dbname = "ble_metabase",
                      dataset_ids = datasetid,
-                     host = 'localhost')) # Tim: replace with An's IP address
+                     host = '10.157.18.83'))
 
 # Read in data from folder
 frompi <- file.path(getwd(), "..", "..", "FromPI")
@@ -32,7 +32,7 @@ clean <- file.path(getwd(), "..")
 # type in username and password in the R console
 metadata <- append_units(get_meta(dbname = "ble_metabase",
                      dataset_ids = datasetid,
-                     host = 'localhost')) # Tim: replace with An's IP address
+                     host = '10.157.18.83'))
 
 # create entities in emld list structure
 entities_datasetid <- create_entity_all(metadata,
