1a84ad68f10df0651afd20bf064f7e3cba2fc43c
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2021-12-17 11:47:49 -0600

    add example Core Program datas as internal dataset

--- R/data.R
+++ R/data.R
@@ -1,3 +1,8 @@
 #' BLE LTER Core Program, information on core sampling stations
 #'
 "station_info"
+
+
+#' example of BLE LTER Core Program data with various formatting issues. use to test functions in this package
+
+"cp_data"
