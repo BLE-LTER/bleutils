be1047b216b45c25e187724c7af7b43a43f0020f
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2022-02-24 16:57:29 -0600

    change sort and order functions to use date_time instead of date

--- R/order_cp_cols.R
+++ R/order_cp_cols.R
@@ -1,5 +1,5 @@
 #' @title Order Core Program data columns
-#' @description Order Core Program data columns in this particular order: node, lagoon, station, season, date_collected/date_time, water_column_position, (insert data columns), station_name, latitude, longitude, habitat_type, station_sampling_priority.
+#' @description Order Core Program data columns in this particular order: node, lagoon, station, season, date_time, water_column_position, (insert data columns), station_name, latitude, longitude, habitat_type, station_sampling_priority.
 #' @param data (data.frame) A data.frame containing Core Program data. Columns names need to be named according to standard CP column names. Use bleutils::reaname_attributes for this to work.
 #' @param type (character) Type of CP data. Choose from "water", "sediment", or "mooring". This affects the columns present.
 #'
@@ -9,7 +9,7 @@ order_cp_cols <- function(data, type){
   begin <- c("node",
              "lagoon",
              "station",
-             if (type %in% c("water", "sediment")) c("season", "date_collected"),
+             if (type %in% c("water", "sediment")) c("season", "date_time"),
              if (type == "mooring") "date_time",
              if (type == "water") "water_column_position")
   end <- c(
