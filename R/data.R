90adaf64be2f515c4190b517456f84a533c838a2
atn38-crwr-d07322 <an.thai.nguyen.38@gmail.com>
2022-04-12 18:01:28 -0500

    fix: something went wrong with station_info, so replacing with stations.rda

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' BLE LTER Core Program, information on core sampling stations
 #'
-"station_info"
+"stations"
 
 
 #' example of BLE LTER Core Program data with various formatting issues. use to test functions in this package
