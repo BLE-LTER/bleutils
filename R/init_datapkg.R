ed3ece407774f11fc7144fd23dff7398b3b8c3d6
atn38-crwr-d07322 <an.thai.nguyen.38@gmail.com>
2022-11-02 17:57:20 -0500

    better checking of available IDs in init_datapkg(), better directory, additional argument data_year

--- R/init_datapkg.R
+++ R/init_datapkg.R
@@ -1,20 +1,28 @@
 #' Create folder structure for a new BLE LTER data package ID
 #' Create these following items: folder structure, R project, reusable R script from template
-#' @param base_path (character) Full file path to parent directory to initiate under. Defaults to "K:/Data", which is "\\austin.utexas.edu/disk/engr/research/crwr/projects/BLE-LTER/Data" mapped to the K drive on An's PC.
+#' @param base_path (character) Full file path to parent directory to initiate under. Defaults to "K:/Data", which is the shared BLE network drive, mapped to the K drive on An's PC.
 #' @param dataset_id (numeric) Dataset ID
 #' @param dataset_nickname (character)
-#'
+#' @param data_year (numeric) The year of data that the generated script will process. E.g. you may be working with 2021 data in 2022. Defaults to the current calendar year if not specified.
+
 #' @return Nothing in R, but new folder structure and R script from template
 #' @export
 
 init_datapkg <- function(dataset_id,
                          dataset_nickname,
-                         base_path = "K:/Data") {
-  stopifnot(dataset_id %% 1 == 0)
+                         base_path = "K:/Data",
+                         data_year = NULL) {
+  # data year is current year if not specified
+  if (is.null(data_year))
+    data_year <- as.integer(format(Sys.Date(), "%Y"))
+  stopifnot(is.integer(dataset_id), is.integer(data_year))
 
-  ## check for exisiting IDs
+  ## check for existing IDs
   dirs <- list.files(base_path)
-  ids <- suppressMessages(as.integer(sapply(dirs, substr, 1, 1)))
+  ids <-
+    suppressMessages(as.integer(sapply(
+      dirs, sub, pattern = "\\_.*", replacement = ""
+    )))
 
   if (dataset_id %in% ids) {
     next_id <- max(ids, na.rm = T) + 1
@@ -32,20 +40,24 @@ init_datapkg <- function(dataset_id,
   if (!dir.exists(file.path(dsdir, "FromPI")))
     dir.create(file.path(dsdir, "FromPI"))
 
-  if (!dir.exists(file.path(dsdir, "Clean"))) {
+  if (!dir.exists(file.path(dsdir, "Clean")))
     dir.create(file.path(dsdir, "Clean"))
-  }
+
   if (!dir.exists(file.path(dsdir, paste0("EML_RProject_", dataset_id)))) {
     dir.create(file.path(dsdir, paste0("EML_RProject_", dataset_id)))
+    dir.create(file.path(dsdir, paste0("EML_RProject_", dataset_id), data_year))
   }
-
-  print(dsdir)
   # write README
-  readme <- readLines(system.file("readme.txt", package = "bleutils"))
+  readme <-
+    readLines(system.file("readme.txt", package = "bleutils"))
   writeLines(readme, file.path(dsdir, "README.txt"))
 
   message(paste("Created directory structure under", dsdir))
 
-  # init data procressing script
-  init_script(dataset_id = dataset_id, workdir = dsdir, type = "init")
+  # init data processing script
+  init_script(
+    dataset_id = dataset_id,
+    file_dir = file.path(dsdir, paste0("EML_RProject_", dataset_id), data_year),
+    type = "init"
+  )
 }
