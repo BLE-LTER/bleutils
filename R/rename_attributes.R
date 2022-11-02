bd281d569d7ee20ce0adda0ad14b3aa04714ed10
atn38-crwr-d07322 <an.thai.nguyen.38@gmail.com>
2022-11-02 17:50:27 -0500

    rename_attributes arguments names more consistent

--- R/rename_attributes.R
+++ R/rename_attributes.R
@@ -6,8 +6,9 @@
 #' @param dataset_id (numeric) A dataset ID.
 #' @param entity (numeric) An entity number.
 #' @param file_dir (character) Path to directory containing flat files (data files). Defaults to current R working directory.
+#' @param file_name (character) File name to
 #' @param x R object. If specified, will skip looking for entity file altogether.
-#' @param file_name
+
 #' @param append_units (logical) If TRUE, will append abbreviated units (see vw_custom_units) to column names as specified in metabase. Defaults to TRUE.
 #'
 #' @return The data table entity with renamed columns.
@@ -22,8 +23,8 @@ rename_attributes <-
            file_name = "",
            x,
            append_units = T) {
-
-    if (!is.null(x)) stopifnot(is.data.frame(x))
+    if (!is.null(x))
+      stopifnot(is.data.frame(x))
 
     # subset to specified dataset_id and entity number
     entity_e <-
@@ -38,12 +39,14 @@ rename_attributes <-
     attributes <-
       subset(meta_list[["attributes"]], datasetid == dataset_id &
                entity_position == entity)
-    units <- subset(meta_list[["unit"]], datasetid == dataset_id)
     if (is.null(x)) {
-    if (filename != "") {
-    entity_df <- data.table::fread(file.path(file_dir, filename))
-  } else entity_df <- data.table::fread(file.path(file_dir, entity_e[["filename"]]))
-    } else entity_df <- x
+      if (file_name != "") {
+        entity_df <- data.table::fread(file.path(file_dir, file_name))
+      } else
+        entity_df <-
+          data.table::fread(file.path(file_dir, entity_e[["filename"]]))
+    } else
+      entity_df <- x
 
     colnames(entity_df) <- attributes[["attributeName"]]
     return(entity_df)
