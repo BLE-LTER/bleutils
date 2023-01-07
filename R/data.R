abfb7dc82571c31fb75b644e2d09789ed3fac0b6
atn38-crwr-d07322 <an.thai.nguyen.38@gmail.com>
2023-01-07 07:42:52 -0600

    feat: remove update_cp_stations, add_cp_cols to read directly from Box shared file

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-#' BLE LTER Core Program, information on core sampling stations
-#'
-"stations"
+#' #' BLE LTER Core Program, information on core sampling stations
+#' #'
+#' "stations"
 
 
 #' example of BLE LTER Core Program data with various formatting issues. use to test functions in this package
--- R/update_cp_stations.R
+++ R/update_cp_stations.R
@@ -1,15 +1,28 @@
-#' Compare the station information to "canonical" source in Box and update if needed. Meant to be run occassionally from An's PC and within the bleutils Rproject to update the version of station info that this package uses to append new columns, for example.
-#' @param source_file (character) Full path to canonical file used to store station codes, names, lat/lons, habitat type. Defaults to "C:/Users/atn893/Documents/Box Sync/Beaufort LTER/Core Program/BLE_LTER_CP_Stations.xlsx" which works on An's PC.
-#' @param sheet (character) Name of Excel sheet containing lookup info. Defaults to "lookup".
-#' @export
-
-update_cp_stations <- function(source_file = "C:/Users/atn893/Documents/Box Sync/Beaufort LTER/Core Program/BLE_LTER_CP_Stations.xlsx",
-                               sheet = "lookup") {
-  canon <- readxl::read_excel(source_file, sheet = sheet)
-  if (exists("stations", where = "package:bleutils") && isTRUE(all.equal(bleutils::stations, canon)) && all(colnames(station_info) == colnames(canon))) message("Package version of station info is up-to-date. No action taken.")
-  else {
-    message("Nope. Something's changed. Updating package version of station info.")
-    stations <- canon
-    usethis::use_data(stations, overwrite = TRUE)
-  }
-}
+#' #' Compare the station information to "canonical" source in Box and update if needed. Meant to be run occassionally from An's PC and within the bleutils Rproject to update the version of station info that this package uses to append new columns, for example.
+#' #'
+#' #' @param source_file (character) Direct URL to file with station codes, names, lat/lons, habitat type. Defaults to "https://utexas.box.com/shared/static/p6a8956afbsfh19422p8xhhtiqmf7eqy.csv" hosted on the "BLE-IM" folder on Box.
+#' #'
+#' #' @export
+#'
+#' update_cp_stations <-
+#'   function(source_file = "https://utexas.box.com/shared/static/p6a8956afbsfh19422p8xhhtiqmf7eqy.csv") {
+#'     try(canon <- read.csv(source_file))
+#'     # canon <- readxl::read_excel(source_file, sheet = sheet)
+#'     if (exists("stations", where = "package:bleutils") &&
+#'         isTRUE(all.equal(bleutils::stations, canon)) &&
+#'         all(colnames(station_info) == colnames(canon)))
+#'       message("Package version of station info is up-to-date. No action taken.")
+#'     else {
+#'       message("Nope. Something's changed. Updating package version of station info.")
+#'       stations <- canon
+#'       if (#package environment
+#'       ) {
+#'         usethis::use_data(stations, overwrite = TRUE)
+#'       } else if () {
+#'         save(stations.)
+#'       }
+#'
+#'       # save(stations, "stations.rda")
+#'       # message("stations have been updated at path:.... Please update on github")
+#'     }
+#'   }
