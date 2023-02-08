d2ca70f2ed18c59106d3e786167434b616429cc8
atn38-crwr-d07322 <an.thai.nguyen.38@gmail.com>
2023-02-08 12:03:33 -0600

    fix: wrong url to csv in add_cp_cols

--- R/add_cp_cols.R
+++ R/add_cp_cols.R
@@ -1,25 +1,29 @@
 #' Add columns relevant to BLE LTER Core Program
-#' Add columns: station name, lat/lon, habitat type, given BLE LTER Core Program station code
+#' @description Add columns: station name, lat/lon, habitat type, given BLE LTER Core Program station code
 #'
 #' @param df (data.frame) Data frame in question with CP data
-#' @param station_source (character) URL or file path to Core Program stations coordinates file. Defaults to https://utexas.box.com/shared/static/p6a8956afbsfh19422p8xhhtiqmf7eqy.csv.
+#' @param station_source (character) URL or file path to Core Program stations coordinates file.
 #' @param station_code_col (character) Name of column containing BLE LTER station codes in supplied df. Defaults to "station".
 #'
 #' @return Data with new station name, lat/lon, habitat type columns appended
 #' @export
 
-add_cp_cols <- function(df, station_code_col = "station", station_source = "https://utexas.box.com/shared/static/p6a8956afbsfh19422p8xhhtiqmf7eqy.csv") {
-  # get stations from online Box file
-  stations <- read.csv(station_source)
-  # check existing data for columns first before merging
-  cols <- c(setdiff(colnames(stations), colnames(df)))
-  if ("station_id" %in% cols) {
-    cols = cols
-  } else {
-    cols <- c(cols, "station_id")
-  }
+add_cp_cols <-
+  function(df,
+           station_code_col = "station",
+           station_source = "https://utexas.box.com/shared/static/sjt5phkdpyx9vsvpvcss461562vb5wsw.csv") {
+    # get stations from online Box file
+    stations <- read.csv(station_source)
+    # check existing data for columns first before merging
+    cols <- c(setdiff(colnames(stations), colnames(df)))
+    if ("station_id" %in% cols) {
+      cols = cols
+    } else {
+      cols <- c(cols, "station_id")
+    }
 
-  # merge
-  df <- merge(df, stations[cols], by.x = station_code_col, by.y = "station_id")
-  return(df)
-}
+    # merge
+    df <-
+      merge(df, stations[cols], by.x = station_code_col, by.y = "station_id")
+    return(df)
+  }
