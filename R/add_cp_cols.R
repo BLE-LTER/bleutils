55dc85a2c1347873c3b836fcaa04b724af1f4e0f
atn38-crwr-d07322 <an.thai.nguyen.38@gmail.com>
2023-04-03 17:36:38 -0500

    solves #4

--- R/add_cp_cols.R
+++ R/add_cp_cols.R
@@ -12,18 +12,35 @@ add_cp_cols <-
   function(df,
            station_code_col = "station",
            station_source = "https://utexas.box.com/shared/static/sjt5phkdpyx9vsvpvcss461562vb5wsw.csv") {
+
     # get stations from online Box file
     stations <- read.csv(station_source)
-    # check existing data for columns first before merging
+
+    # check existing data for CP columns (e.g. if someone had already filled in the node column)
     cols <- c(setdiff(colnames(stations), colnames(df)))
-    if ("station_id" %in% cols) {
-      cols = cols
-    } else {
+    if (!"station_id" %in% cols) {
       cols <- c(cols, "station_id")
     }
 
-    # merge
+    # warn if there are un-matched stations
+    stations_df <- unique(df[[station_code_col]])
+    stations_source <- unique(stations$station_id)
+
+    if (!all(stations_df %in% stations_source)) {
+      unmatched <- stations_df[!(stations_df %in% stations_source)]
+      warning(paste0("There are stations in the supplied data frame that are not present in the reference source. They are: \n",
+                    paste0(unmatched, collapse = "\n"),
+                    "\n Rows with these unmatched stations have been retained with NAs in the additional columns. You may want to check the spelling of the station codes, for example: are Stefasson Sound stations still prefixed STD? We renamed those to SSL in 2022. For the reference source, see the Box file at https://utexas.app.box.com/file/1092468994724?s=sjt5phkdpyx9vsvpvcss461562vb5wsw. Contact An or Tim to add your unmatched stations to the reference source."))
+    } else {
+      message("All stations in the supplied data frame have matches in the reference source.")
+    }
+
+    # merge, keeping un-matched stations
     df <-
-      merge(df, stations[cols], by.x = station_code_col, by.y = "station_id")
+      merge(df,
+            stations[cols],
+            by.x = station_code_col,
+            by.y = "station_id",
+            all.x = TRUE)
     return(df)
   }
