29092d0f43c219d21dfb027d675a04a33410e2ca
Tim Whiteaker <whiteaker@utexas.edu>
2023-09-21 12:28:18 -0500

    raise error when ordering CP columns if not all columns present

--- R/order_cp_cols.R
+++ R/order_cp_cols.R
@@ -1,5 +1,5 @@
 #' @title Order Core Program data columns
-#' @description Order Core Program data columns in this particular order: node, lagoon, station, season, date_time, water_column_position, (insert data columns), station_name, latitude, longitude, habitat_type, station_sampling_priority.
+#' @description Order Core Program data columns in this particular order: node, lagoon, station, season, date_time, water_column_position, (insert data columns), station_name, latitude, longitude, station_depth, habitat_type, station_sampling_priority.
 #' @param data (data.frame) A data.frame containing Core Program data. Columns names need to be named according to standard CP column names. Use \code{bleutils::rename_attributes} prior to running this to rename columns to what's in metadata.
 #' @param type (character) Type of CP data. Choose from "water", "sediment", or "mooring". Use "sediment" for biota data as well.
 #'
@@ -17,10 +17,19 @@ order_cp_cols <- function(data, type){
     "station_name",
     "latitude",
     "longitude",
+    if (type == "water") "station_depth",
     "habitat_type",
     "station_sampling_priority")
 
   data_cols <- setdiff(colnames(data), c(begin, end))
+
+  # Check for missing required columns
+  missing_begin <- setdiff(begin, colnames(data))
+  missing_end <- setdiff(end, colnames(data))
+  missing_columns <- union(missing_begin, missing_end)
+  if (length(missing_columns) > 0) {
+    stop(paste("The following columns are missing:", paste(missing_columns, collapse = ", ")))
+  }
 
   data <- subset(data, select = c(begin, data_cols, end))
   return(data)
