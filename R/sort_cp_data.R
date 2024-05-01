233d0aa26a62558b673f641df32bc2cce8a25431
Induja <induja.mohandas@austin.utexas.edu>
2024-05-01 09:35:57 -0500

    updated code to sort with additional columns

--- R/sort_cp_data.R
+++ R/sort_cp_data.R
@@ -7,26 +7,26 @@
 #'
 #' @export
 
-sort_cp_rows <- function(data, type){
+sort_cp_rows <- function(data, type) {
   stopifnot(c("node", "lagoon", "station") %in% colnames(data))
- sort_by <- order(data[["node"]],
-                  data[["lagoon"]],
-                  data[["station"]])
-  if (type == "water") {
-    sort_by <- order(data[["node"]],
-                     data[["lagoon"]],
-                     data[["station"]],
-                     data[["date_time"]],
-                     data[["water_column_position"]])
-  } else if (type == "sediment") {
-    sort_by <- order(data[["node"]],
-                     data[["lagoon"]],
-                     data[["station"]],
-                     data[["date_time"]])
-  } else if (type == "mooring") {
-    sort_by <- order(data[["station"]],
-                     data[["date_time"]])
-  }
- data <- data[sort_by, ]
+  
+  # Define columns to sort by based on type
+  sort_columns <- switch(
+    type,
+    "water" = c("node", "lagoon", "station", "date_time", "water_column_position", "wavelength_nm"),
+    "sediment" = c("node", "lagoon", "station", "date_time"),
+    "mooring" = c("station", "date_time"),
+    stop("Invalid type")
+  )
+  
+  # Filter sort_columns to only include existing columns in the dataframe
+  sort_columns <- intersect(sort_columns, colnames(data))
+  
+  # Create vector of columns to sort
+  sort_by <- do.call("order", data[sort_columns])
+  
+  # Sort the dataframe
+  data <- data[sort_by, ]
+  
   return(data)
 }
